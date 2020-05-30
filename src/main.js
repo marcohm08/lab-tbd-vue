ef091cc575ee88c72967893ce650a718e8bfd95a
Marco Hernandez <marco.hernandez.m@usach.cl>
2020-05-30 19:32:51 -0400

    Agregado axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+//Instancia de axios se configura con URL base del backend
+const axiosInstance = axios.create({ 
+  baseURL: 'http://localhost:8080'
+})
+
+//Para acceder a axios desde this.$http
+Vue.prototype.$http = axiosInstance;
 
 new Vue({
   router,
