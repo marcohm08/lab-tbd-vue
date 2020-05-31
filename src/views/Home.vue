0fe7f0916f025360844f048f38f807eac8730a48
Marco Hernandez <marco.hernandez.m@usach.cl>
2020-05-30 21:33:49 -0400

    Se despliega la info de la historia de usuario2 en Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class=" container mt-5">
+    <h2 class="mb-3">Busqueda de voluntario segun emergencia </h2>
+    <div class="row mx-5 my-3">
+      <input type="text" placeholder="Ingrese nombre de la emergencia" class="form-control mx-3" style="width: 50%" v-model="nombreEmergencia">
+      <button class="btn btn-primary" @click="obtenerVoluntarios(nombreEmergencia)">Buscar</button>
+    </div>
+    <div v-if="voluntarios.length != 0">
+      <table class="table table-fixed">
+        <thead>
+          <tr>
+            <th class="col-xs-3">Nombre</th>
+            <th class="col-xs-3">Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(voluntario,index) in voluntarios" :key="index">
+            <td class="col-xs-3">{{voluntario.id}}</td>
+            <td class="col-xs-3">{{voluntario.nombre}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      nombreEmergencia: '',
+      voluntarios: []
+    }
+  },
+  methods:{
+    obtenerVoluntarios(nombre){
+      if(nombre != ''){
+        this.$http.get("/voluntario/nombre-emergencia/"+nombre).then(response => {
+          this.voluntarios = response.data
+        }).catch(error => {console.log(error.message)})
+
+      }
+    }
   }
+
+  
 }
 </script>
