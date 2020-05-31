a23450232e613fe3f75959a0cfd3b1487d6c616c
AlexMunozP <alex.munoz@usach.cl>
2020-05-31 15:18:54 -0400

    Historia de usuario 1

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,12 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+    {
+      path: '/voluntario/habilidad',
+      name: 'VoluntarioHabilidad',
+      component: () => import(/* webpackChunkName: "about" */ '../views/VoluntarioHabilidad.vue')
+    }
 ]
 
 const router = new VueRouter({
---
+++ src/views/VoluntarioHabilidad.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class=" container mt-5">
+        <h2 class="mb-3">Busqueda de voluntario segun habilidad </h2>
+        <div class="row mx-5 my-3">
+            <input type="text" placeholder="Ingrese id de la habilidad" class="form-control mx-3" style="width: 50%" v-model="idHabilidad">
+            <button class="btn btn-primary" @click="obtenerVoluntarios(idHabilidad)">Buscar</button>
+        </div>
+        <div v-if="voluntarios.length != 0">
+            <table class="table table-fixed">
+                <thead>
+                <tr>
+                    <th class="col-xs-3">Nombre</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(voluntario,index) in voluntarios" :key="index">
+                    <td class="col-xs-3">{{voluntario.nombre}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: 'VoluntarioHabilidad',
+        data(){
+            return {
+                nombreHabilidad: '',
+                voluntarios: []
+            }
+        },
+        methods:{
+            obtenerVoluntarios(id){
+                if(id != ''){
+                    this.$http.get("voluntario/habilidad/"+ id).then(response => {
+                        this.voluntarios = response.data
+                    }).catch(error => {console.log(error.message)})
+
+                }
+            }
+        }
+
+
+    }
+</script>
