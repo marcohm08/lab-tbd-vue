c708f40435fadf66fb456942d9577b7bd89ac6dc
Marco <marco.hernandez.m@usach.cl>
2020-08-08 17:46:57 -0400

    Agregados componentes de paginacion * Cambiado formato de la tabla de voluntarios

---
+++ src/components/Pagination/BasePagination.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="row">
+    <a class="btn btn-success"
+      :disabled="isPreviousButtonDisabled"
+      @click.native="previousPage"
+    >
+      ←
+    </a>
+    <a class="btn btn-success"
+      :disabled="isNextButtonDisabled"
+      @click.native="nextPage"
+    >
+      →
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    pageCount: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    isPreviousButtonDisabled() {
+      return this.currentPage === 1
+    },
+    isNextButtonDisabled() {
+      return this.currentPage === this.pageCount
+    }
+  },
+  methods: {
+    nextPage() {
+      this.$emit('nextPage')
+    },
+    previousPage() {
+      this.$emit('previousPage')
+    }
+  }
+}
+</script>
--- src/views/VoluntarioEmeHabilidad.vue
+++ src/views/VoluntarioEmeHabilidad.vue
@@ -1,62 +1,91 @@
 <template>
     <div class=" container mt-5">
         <h2 class="mb-3">Busqueda de voluntario según emergencia y/o habilidad</h2>
-        <div class="row mx-5 my-3">
-            <input type="text" placeholder="Ingrese nombre de la emergencia" class="form-control mx-3" style="width: 50%" v-model="nombreEmergencia">
+        <div class="row mx-5 mb-3 mt-5">
+
+            <!-- <input type="text" placeholder="Ingrese nombre de la emergencia" class="form-control mx-3" style="width: 50%" v-model="nombreEmergencia">
             <input type="text" placeholder="Ingrese nombre de la habilidad" class="form-control mx-3" style="width: 50%" v-model="idHabilidad">
-            <button class="btn btn-primary" @click="obtenerVoluntarios(nombreEmergencia, idHabilidad)">Buscar</button>
+            <button class="btn btn-primary" @click="obtenerVoluntarios(nombreEmergencia, idHabilidad)">Buscar</button> -->
         </div>
         <div v-if="voluntarios.length !== 0">
-            <table class="table table-fixed">
+            <table class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
                 <thead>
                 <tr>
+                    <th class="col-xs-3">Identificador</th>
                     <th class="col-xs-3">Nombre</th>
-                    <th class="col-xs-3">Last Name</th>
+                    <th class="col-xs-3">Fecha de nacimiento</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr v-for="(voluntario,index) in voluntarios" :key="index">
                     <td class="col-xs-3">{{voluntario.id}}</td>
                     <td class="col-xs-3">{{voluntario.nombre}}</td>
+                    <td class="col-xs-3">{{voluntario.fnacimiento}}</td>
                 </tr>
                 </tbody>
             </table>
+            <pagination class="my-3" 
+            :current-page="paginaActual" 
+            :pagecount="cuentaPagina"
+            @nextPage="cambiaPagina('siguiente')"
+            @previusPage="cambiaPagina('anterior')" />
         </div>
     </div>
 </template>
 
 <script>
-
-
-    export default {
-        name: 'voluntario-habilidad',
-        data(){
-            return {
-                nombreEmergencia: '',
-                idHabilidad: '',
-                voluntarios: []
+  import BasePagination from '@/components/Pagination/BasePagination.vue'
+  export default {
+    components:{
+      'pagination': BasePagination
+    },
+    name: 'voluntario-habilidad',
+    data(){
+        return {
+            nombreEmergencia: '',
+            idHabilidad: '',
+            voluntarios: [],
+            paginaActual: 1,
+            cuentaPagina: 0
+        }
+    },
+    methods:{
+        obtenerVoluntarios(nombre, id){
+            if(nombre !== '' && id === ''){
+                this.$http.get("/voluntario/nombre-emergencia/"+nombre).then(response => {
+                    this.voluntarios = response.data
+                }).catch(error => {console.log(error.message)})
+            }
+            else if(nombre === '' & id !== ''){
+                this.$http.get("/voluntario/habilidad/"+id).then(response => {
+                    this.voluntarios = response.data
+                }).catch(error => {console.log(error.message)})
+            }
+            else{
+                this.$http.get("/voluntario/emergencia/"+nombre+"/habilidad/"+id).then(response => {
+                    this.voluntarios = response.data
+                }).catch(error => {console.log(error.message)})
             }
         },
-        methods:{
-            obtenerVoluntarios(nombre, id){
-                if(nombre !== '' && id === ''){
-                    this.$http.get("/voluntario/nombre-emergencia/"+nombre).then(response => {
-                        this.voluntarios = response.data
-                    }).catch(error => {console.log(error.message)})
-                }
-                else if(nombre === '' & id !== ''){
-                    this.$http.get("/voluntario/habilidad/"+id).then(response => {
-                        this.voluntarios = response.data
-                    }).catch(error => {console.log(error.message)})
-                }
-                else{
-                    this.$http.get("/voluntario/emergencia/"+nombre+"/habilidad/"+id).then(response => {
-                        this.voluntarios = response.data
-                    }).catch(error => {console.log(error.message)})
-                }
+        async cambiaPagina(valor){
+            switch (valor) {
+                case 'siguiente':
+                    this.paginaActual += 1
+                    break
+                case 'anterior' :
+                    this.paginaActual -= 1
+                    break
+                default:
+                    this.paginaActual = valor
             }
         }
-
-
+    },
+    created(){
+        this.$http.get("/voluntario").then(response => {
+            this.voluntarios = response.data
+        }).catch(error => {console.log(error.message)})
     }
+
+
+  }
 </script>
